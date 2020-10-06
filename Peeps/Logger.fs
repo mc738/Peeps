39721961a81d96d1ab935850f981d70b29ad2195
Max <me.mclifford.dev>
2020-10-06 23:38:45 +0100

    Initial commit

---
+++ Peeps/Logger.fs
@@ -0,0 +1,53 @@
+namespace Peeps
+
+open System
+
+type LogItem =
+    { from: string
+      message: string
+      time: DateTime
+      ``type``: ItemType }
+
+and ItemType =
+    | Success
+    | Error
+    | Information
+    | Warning
+    | Debug
+
+/// A basic logging class.
+type Logger() =
+
+    let getCCT itemType =
+        match itemType with
+        | Success -> (ConsoleColor.Green, "OK")
+        | Error -> (ConsoleColor.Red, "ERROR")
+        | Information -> (ConsoleColor.White, "INFO")
+        | Warning -> (ConsoleColor.Yellow, "WARN")
+        | Debug -> (ConsoleColor.Magenta, "DEBUG")
+
+    let handleItem item = 
+        let (color, title) = getCCT item.``type``
+        Console.ForegroundColor <- color
+        printf "%s\t" title
+        Console.ResetColor()
+        let time = item.time.ToString()
+        printfn "[%s] %s: %s" time item.from item.message
+        true
+ 
+    let listener =
+        MailboxProcessor<LogItem>
+            .Start(fun inbox ->
+                  let rec loop () =
+                      async {
+
+                          let! item = inbox.Receive()
+
+                          let cont = handleItem item
+
+                          if cont then return! loop ()
+                      }
+
+                  loop ())
+ 
+    member this.Post item = listener.Post item
