ae7eda0ebb915b32db6be2070b0f3e2c80ab5e24
Max Clifford <me@mclifford.dev>
2022-07-18 21:09:11 +0100

    Added documentation

--- Peeps/Extensions.fs
+++ Peeps/Extensions.fs
@@ -1,18 +1,21 @@
-namespace Peeps.Extensions
-
-open Microsoft.Extensions.Logging
-open Peeps.Logger
+namespace Peeps
 
 [<AutoOpen>]
-module LoggingBuilder =
+module Extensions =
 
-    type ILoggingBuilder with
+    open Microsoft.Extensions.Logging
+    open Peeps.Logger
 
-        member builder.AddPeeps(ctx : PeepsContext) =
-            [ LoggerConfig.InfoConfig(ctx.Actions, 0)
-              LoggerConfig.DebugConfig(ctx.Actions, 0)
-              LoggerConfig.TraceConfig(ctx.Actions, 0)
-              LoggerConfig.WarningConfig(ctx.Actions, 0)
-              LoggerConfig.ErrorConfig(ctx.Actions, 0)
-              LoggerConfig.CriticalConfig(ctx.Actions, 0) ]
-            |> List.fold (fun (b: ILoggingBuilder) config -> b.AddProvider(new LoggerProvider(config))) builder
+    [<AutoOpen>]
+    module LoggingBuilder =
+
+        type ILoggingBuilder with
+
+            member builder.AddPeeps(ctx : PeepsContext) =
+                [ LoggerConfig.InfoConfig(ctx.Actions, 0)
+                  LoggerConfig.DebugConfig(ctx.Actions, 0)
+                  LoggerConfig.TraceConfig(ctx.Actions, 0)
+                  LoggerConfig.WarningConfig(ctx.Actions, 0)
+                  LoggerConfig.ErrorConfig(ctx.Actions, 0)
+                  LoggerConfig.CriticalConfig(ctx.Actions, 0) ]
+                |> List.fold (fun (b: ILoggingBuilder) config -> b.AddProvider(new LoggerProvider(config))) builder
--- Peeps/Logger.fs
+++ Peeps/Logger.fs
@@ -1,74 +1,79 @@
-namespace Peeps.Logger
+namespace Peeps
 
-open System
-open System.Collections.Concurrent
-open Microsoft.Extensions.Logging
-open Peeps.Core
+module Logger =
 
-type LoggerAction = PeepsLogItem -> unit
+    open System
+    open System.Collections.Concurrent
+    open Microsoft.Extensions.Logging
+    open Peeps.Core
 
-type LoggerConfig =
-    { EventId: int
-      LogLevel: LogLevel
-      Actions: LoggerAction list }
-    static member Create(logLevel, actions, eventId) =
-        { EventId = eventId
-          LogLevel = logLevel
-          Actions = actions }
+    type LoggerAction = PeepsLogItem -> unit
 
-    static member InfoConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Information, actions, eventId)
+    /// <summary>The logger configuration.</summary>
+    type LoggerConfig =
+        { EventId: int
+          LogLevel: LogLevel
+          Actions: LoggerAction list }
+        
+        /// <summary>Create a new configuration.</summary>
+        static member Create(logLevel, actions, eventId) =
+            { EventId = eventId
+              LogLevel = logLevel
+              Actions = actions }
 
-    static member DebugConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Debug, actions, eventId)
+        static member InfoConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Information, actions, eventId)
 
-    static member TraceConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Trace, actions, eventId)
+        static member DebugConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Debug, actions, eventId)
 
-    static member ErrorConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Error, actions, eventId)
+        static member TraceConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Trace, actions, eventId)
 
-    static member WarningConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Warning, actions, eventId)
+        static member ErrorConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Error, actions, eventId)
 
-    static member CriticalConfig(actions, eventId) =
-        LoggerConfig.Create(LogLevel.Critical, actions, eventId)
+        static member WarningConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Warning, actions, eventId)
 
-type Logger(name: string, config: LoggerConfig) =
+        static member CriticalConfig(actions, eventId) =
+            LoggerConfig.Create(LogLevel.Critical, actions, eventId)
 
-    interface ILogger with
-        member this.BeginScope(state) = Unchecked.defaultof<IDisposable>
-        member this.IsEnabled(logLevel) = logLevel = config.LogLevel
+    type Logger(name: string, config: LoggerConfig) =
 
-        member this.Log(logLevel, eventId, state, ``exception``, formatter) =
-            match (this :> ILogger).IsEnabled(logLevel)
-                  && (config.EventId = 0 || config.EventId = eventId.Id) with
-            | true ->
-                // create the peeps log item
-                let message =
-                    $"[%i{eventId.Id} %s{logLevel.ToString()}] %s{name} - %s{formatter.Invoke(state, ``exception``)}"
+        interface ILogger with
+            member this.BeginScope(state) = Unchecked.defaultof<IDisposable>
+            member this.IsEnabled(logLevel) = logLevel = config.LogLevel
 
-                let item =
-                    PeepsLogItem.Create(logLevel, name, message)
-                
-                config.Actions |> List.iter (fun a -> a item)
-            | false -> () // Do nothing.
+            member this.Log(logLevel, eventId, state, ``exception``, formatter) =
+                match (this :> ILogger).IsEnabled(logLevel)
+                      && (config.EventId = 0 || config.EventId = eventId.Id) with
+                | true ->
+                    // create the peeps log item
+                    let message =
+                        $"[%i{eventId.Id} %s{logLevel.ToString()}] %s{name} - %s{formatter.Invoke(state, ``exception``)}"
 
-type LoggerProvider(config: LoggerConfig) =
+                    let item =
+                        PeepsLogItem.Create(logLevel, name, message)
+                    
+                    config.Actions |> List.iter (fun a -> a item)
+                | false -> () // Do nothing.
 
-    let mutable _loggers = ConcurrentDictionary<string, Logger>()
+    type LoggerProvider(config: LoggerConfig) =
 
-    interface ILoggerProvider with
-        member this.CreateLogger(categoryName) =
-            _loggers.GetOrAdd(categoryName, (fun name -> new Logger(name, config))) :> ILogger
+        let mutable _loggers = ConcurrentDictionary<string, Logger>()
 
-        member this.Dispose() = _loggers.Clear()
+        interface ILoggerProvider with
+            member this.CreateLogger(categoryName) =
+                _loggers.GetOrAdd(categoryName, (fun name -> new Logger(name, config))) :> ILogger
 
-type PeepsContext =
-    { Name: string
-      OutputDirectory: string
-      Actions: LoggerAction list }
-    static member Create(outputDirectory, name, actions) =
-        { Name = name
-          OutputDirectory = outputDirectory
-          Actions = actions }
+            member this.Dispose() = _loggers.Clear()
+
+    type PeepsContext =
+        { Name: string
+          OutputDirectory: string
+          Actions: LoggerAction list }
+        static member Create(outputDirectory, name, actions) =
+            { Name = name
+              OutputDirectory = outputDirectory
+              Actions = actions }
